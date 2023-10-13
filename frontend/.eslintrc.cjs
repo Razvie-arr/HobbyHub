03a7085ae67715942126de5c128f125cdb349599
trah07 <trah07@vse.cz>
2023-10-13 16:35:11 +0200

    yarn version + eslint fix

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -53,6 +53,7 @@ module.exports = {
       parser: '@typescript-eslint/parser',
       parserOptions: {
         project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
       },
       files: ['**/*.ts', '**/*.tsx'],
       rules: {
