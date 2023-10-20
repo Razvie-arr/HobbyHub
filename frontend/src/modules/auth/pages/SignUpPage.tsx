329a7f4edcd0a368bdedae307b04ab701710e467
Sabina Balaeva <sabibalaeva@gmail.com>
2023-10-20 03:09:51 +0200

    added sign up form base

--- frontend/src/modules/auth/pages/SignUpPage.tsx
+++ frontend/src/modules/auth/pages/SignUpPage.tsx
@@ -1,9 +1,10 @@
 import { useMutation } from '@apollo/client';
+import { FormControl, FormLabel, Text } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 
 import { gql } from 'src/gql';
 import { useAuth } from 'src/modules/auth';
-import { Box, Button } from 'src/shared/design-system';
+import { Box, Button, Flex, Input, Stack } from 'src/shared/design-system';
 
 const SIGNUP_MUTATION = gql(/* GraphQL */ `
   mutation SignUp($email: String!, $name: String!, $password: String!) {
@@ -30,23 +31,64 @@ export function SignUpPage() {
   });
 
   return (
-    <>
-      <Button
-        onClick={() => {
-          void signUpRequest({
-            variables: {
-              email: 'a@a.com',
-              name: 'John Doe',
-              password: 'pass',
-            },
-          });
-        }}
-        isLoading={signUpRequestState.loading}
-      >
-        Sign Up
-      </Button>
-      {signUpRequestState.error ? <Box color="red">{signUpRequestState.error.message}</Box> : null}
-    </>
+      <Flex
+        minH='100vh'
+        align='center'
+        justify='center'
+        bg='purple.50'>
+        <Stack spacing={4} mx='auto' minW='lg' maxW='lg' py={12} px={6}>
+        <Text mx='auto' fontSize='2xl' fontWeight='bold'>Create your account</Text>
+          <Box
+            rounded='lg'
+            bg='white'
+            boxShadow='lg'
+            p={8}>
+            <Stack spacing={4}>
+              <FormControl id="firstName">
+                <FormLabel>First name</FormLabel>
+                <Input type="text" />
+              </FormControl>
+              <FormControl id="lastName">
+                <FormLabel>Last name</FormLabel>
+                <Input type="text" />
+              </FormControl>
+              <FormControl id="userName">
+                <FormLabel>Username</FormLabel>
+                <Input type="text" />
+              </FormControl>
+              <FormControl id="email">
+                <FormLabel>Email address</FormLabel>
+                <Input type="email" />
+              </FormControl>
+              <FormControl id="password">
+                <FormLabel>Password</FormLabel>
+                <Input type="password" />
+              </FormControl>
+              <FormControl id="password">
+                <FormLabel>Password confirmation</FormLabel>
+                <Input type="password" />
+              </FormControl>
+
+              <Button
+                colorScheme="purple"
+                onClick={() => {
+                  void signUpRequest({
+                    variables: {
+                      email: 'a@a.com',
+                      name: 'John Doe',
+                      password: 'pass',
+                    },
+                  });
+                }}
+                isLoading={signUpRequestState.loading}
+                >
+                Sign up
+              </Button>
+              {signUpRequestState.error ? <Box color="red">{signUpRequestState.error.message}</Box> : null}
+            </Stack>
+          </Box>
+        </Stack>
+      </Flex>
   );
 }
 
