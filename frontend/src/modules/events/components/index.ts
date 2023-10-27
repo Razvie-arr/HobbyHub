0a201cce2fc56fec49cdb596ee6006289b54b67f
Sabina Balaeva <sabibalaeva@gmail.com>
2023-10-27 07:59:14 +0000

    feat: added create event form

--- frontend/src/modules/events/components/index.ts
+++ frontend/src/modules/events/components/index.ts
@@ -1,3 +1,5 @@
+export { EventCard } from './EventCard';
+export { EventForm } from './EventForm';
 export { EventsMapButton } from './EventsMapButton';
 export { EventsSection } from './EventsSection';
 
