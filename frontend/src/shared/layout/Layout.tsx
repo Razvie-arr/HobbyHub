3997e716b60241e5b97ddd21ba703269903640d0
Le Anh Viet Linh <lav.linh@gmail.com>
2023-10-29 19:02:42 +0100

    fix: remove onboarding banner when not signed in

--- frontend/src/shared/layout/Layout.tsx
+++ frontend/src/shared/layout/Layout.tsx
@@ -4,31 +4,37 @@ import { HStack, Link, Text } from '@chakra-ui/react';
 import { Alert, AlertIcon, Box, Flex } from 'src/shared/design-system';
 import { Footer, ReactRouterLink, TopNavigation } from 'src/shared/navigation';
 
+import { useAuth } from '../../modules/auth';
 import { route } from '../../route';
 
 import { ContentContainer } from './ContentContainer';
 
-export const Layout = ({ children }: PropsWithChildren) => (
-  <Flex direction="column" minHeight="100vh">
-    <Alert status="warning">
-      <ContentContainer>
-        <HStack justifyContent="center" alignItems="center">
-          <AlertIcon />
-          <Text>
-            Click{' '}
-            <Link as={ReactRouterLink} to={route.onboarding()} color="purple.500">
-              here{' '}
-            </Link>
-            to finish your onboarding and unlock a personalized experience on HobbyHub!
-          </Text>
-        </HStack>
-      </ContentContainer>
-    </Alert>
-    <TopNavigation />
-    <Box bg="purple.50" flexGrow={1}>
-      {children}
-    </Box>
-    <Footer />
-  </Flex>
-);
+export const Layout = ({ children }: PropsWithChildren) => {
+  const { user } = useAuth();
+  return (
+    <Flex direction="column" minHeight="100vh">
+      {user && (user.event_types.length === 0 || !user.location) ? (
+        <Alert status="warning">
+          <ContentContainer>
+            <HStack justifyContent="center" alignItems="center">
+              <AlertIcon />
+              <Text>
+                Click{' '}
+                <Link as={ReactRouterLink} to={route.onboarding()} color="purple.500">
+                  here{' '}
+                </Link>
+                to finish your onboarding and unlock a personalized experience on HobbyHub!
+              </Text>
+            </HStack>
+          </ContentContainer>
+        </Alert>
+      ) : null}
+      <TopNavigation />
+      <Box bg="purple.50" flexGrow={1}>
+        {children}
+      </Box>
+      <Footer />
+    </Flex>
+  );
+};
 
