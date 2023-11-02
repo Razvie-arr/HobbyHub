4313483e7c654b3eed1d764204c15b65bdf9f624
Le Anh Viet Linh <lav.linh@gmail.com>
2023-11-02 17:05:58 +0100

    fix: fixed types

--- frontend/src/shared/filters/index.ts
+++ frontend/src/shared/filters/index.ts
@@ -1,3 +1,4 @@
-export { MainFilters, type MainFiltersValues } from './MainFilters';
+export { MainFilters } from './MainFilters';
+export { type MainFiltersValues } from './types';
 export { createFilterValuesFromParams } from './utils';
 
