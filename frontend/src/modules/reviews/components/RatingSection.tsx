ea1e11201e68405ba8721f5d064c02efe3a0ae3c
Le Anh Viet Linh <lav.linh@gmail.com>
2023-12-02 16:19:29 +0100

    feat: added slider marks

--- frontend/src/modules/reviews/components/RatingSection.tsx
+++ frontend/src/modules/reviews/components/RatingSection.tsx
@@ -1,16 +1,39 @@
-import { Flex, Stack } from '@chakra-ui/react';
+import { Flex, SliderMark, Stack } from '@chakra-ui/react';
 import { useFormContext } from 'react-hook-form';
 
 import { SliderField } from '../../../shared/forms';
 
 import { StarRating } from './StarRating';
 
+const labelStyles = {
+  mt: '-10',
+  fontWeight: 'bold',
+};
+
 export const RatingSection = () => {
   const { watch } = useFormContext();
   const rating = watch('rating');
   return (
     <Stack>
-      <SliderField name="rating" defaultValue={0} min={0} max={5} step={0.1} />
+      <SliderField
+        sliderMarks={
+          <>
+            {' '}
+            <SliderMark value={0} {...labelStyles}>
+              Poor
+            </SliderMark>
+            <SliderMark value={4.17} {...labelStyles}>
+              Excellent
+            </SliderMark>
+          </>
+        }
+        name="rating"
+        defaultValue={0}
+        min={0}
+        max={5}
+        step={0.1}
+        mt="10"
+      />
       <Stack direction="row" justifyContent="space-between" spacing={0}>
         <StarRating rating={rating} />
         <Flex
--- frontend/src/shared/forms/molecules/fields/SliderField.tsx
+++ frontend/src/shared/forms/molecules/fields/SliderField.tsx
@@ -1,14 +1,16 @@
+import { ReactNode } from 'react';
 import { Slider, SliderFilledTrack, SliderProps, SliderThumb, SliderTrack } from '@chakra-ui/react';
 
 import { FormField, type FormFieldBaseProps } from '../FormField';
 
-export type SliderFieldProps = FormFieldBaseProps<SliderProps>;
+export type SliderFieldProps = FormFieldBaseProps<SliderProps> & { sliderMarks: ReactNode };
 
-export function SliderField({ id, name, label, ...sliderProps }: SliderFieldProps) {
+export function SliderField({ id, name, label, sliderMarks, ...sliderProps }: SliderFieldProps) {
   return (
     <FormField id={id} name={name} label={label} isRequired={sliderProps.isRequired}>
       {(field) => (
         <Slider {...sliderProps} {...field} aria-label="slider">
+          {sliderMarks}
           <SliderTrack>
             <SliderFilledTrack />
           </SliderTrack>
