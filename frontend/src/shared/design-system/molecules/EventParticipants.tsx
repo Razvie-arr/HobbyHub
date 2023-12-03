8e3afaef9aaf0d688aa1662f6e17ed7a2f3eade5
Le Anh Viet Linh <lav.linh@gmail.com>
2023-12-03 23:06:08 +0100

    feat: count only accepted participants

--- frontend/src/shared/design-system/molecules/EventParticipants.tsx
+++ frontend/src/shared/design-system/molecules/EventParticipants.tsx
@@ -1,6 +1,7 @@
 import { Icon, Stack, Text, TypographyProps } from '@chakra-ui/react';
 import { FaPeopleGroup } from 'react-icons/fa6';
 
+import { ParticipantState } from '../../../gql/graphql';
 import { EventData } from '../../types';
 
 interface EventParticipantsProps {
@@ -10,11 +11,17 @@ interface EventParticipantsProps {
   participants: EventData['participants'];
 }
 
-export const EventParticipants = ({ noIcon, fontSize = 'sm', capacity, participants }: EventParticipantsProps) => (
-  <Stack direction="row">
-    {noIcon ? null : <Icon as={FaPeopleGroup} color="purple.500" />}
-    <Text fontWeight="medium" fontSize={fontSize}>
-      {`${participants.length} participant${participants.length === 1 ? '' : 's'} / ${capacity}`}
-    </Text>
-  </Stack>
-);
+export const EventParticipants = ({ noIcon, fontSize = 'sm', capacity, participants }: EventParticipantsProps) => {
+  const acceptedParticipants = participants.filter(({ state }) => state === ParticipantState.Accepted);
+  return (
+    <Stack direction="row">
+      {noIcon ? null : <Icon as={FaPeopleGroup} color="purple.500" />}
+      <Text fontWeight="medium" fontSize={fontSize}>
+        {`${acceptedParticipants.length} participant${acceptedParticipants.length === 1 ? '' : 's'} / ${
+          capacity as number
+        }`}
+      </Text>
+    </Stack>
+  );
+};
+
