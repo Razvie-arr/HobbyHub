4c539bbeab2af297203c29b64fb1d3316df71fc3
Le Anh Viet Linh <lav.linh@gmail.com>
2023-12-04 12:28:58 +0100

    fix: fixed dom warning

--- frontend/src/modules/events/components/shared/EditEventButton.tsx
+++ frontend/src/modules/events/components/shared/EditEventButton.tsx
@@ -1,15 +1,32 @@
 import { Button, ButtonProps } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { match } from 'ts-pattern';
 
 import { route } from '../../../../route';
 
 interface EditEventButtonProps extends ButtonProps {
   eventId: number;
+  as?: 'button' | 'link';
 }
 
-export const EditEventButton = ({ eventId, ...buttonProps }: EditEventButtonProps) => (
-  <Button as={Link} to={route.editEvent(eventId)} {...buttonProps}>
-    Edit
-  </Button>
-);
+export const EditEventButton = ({ eventId, as = 'button', ...buttonProps }: EditEventButtonProps) => {
+  const navigate = useNavigate();
+  return match(as)
+    .with('button', () => (
+      <Button
+        {...buttonProps}
+        onClick={() => {
+          navigate(route.editEvent(eventId));
+        }}
+      >
+        Edit
+      </Button>
+    ))
+    .with('link', () => (
+      <Button as={Link} to={route.editEvent(eventId)} {...buttonProps}>
+        Edit
+      </Button>
+    ))
+    .exhaustive();
+};
 
