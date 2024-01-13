57254a8680734df39570e5459a8a2bc0eba03233
Le Anh Viet Linh <lav.linh@gmail.com>
2024-01-13 15:28:42 +0100

    fix: added links to event categories links

--- frontend/src/modules/landing/components/EventCategories.tsx
+++ frontend/src/modules/landing/components/EventCategories.tsx
@@ -1,12 +1,15 @@
 import { PropsWithChildren } from 'react';
 import { Box, Card, CardBody, Flex, Heading, Icon, Image, Text, VStack } from '@chakra-ui/react';
 import { MdChevronRight } from 'react-icons/md';
+import { To } from 'react-router-dom';
 
+import { route } from '../../../route';
 import { ReactRouterLink } from '../../../shared/navigation';
 
-const EventCategoryLink = ({ children }: PropsWithChildren<{}>) => (
+const EventCategoryLink = ({ children, to }: PropsWithChildren<{ to: To }>) => (
   <Text
     as={ReactRouterLink}
+    to={to}
     fontSize="2xl"
     display="flex"
     justifyContent="center"
@@ -28,19 +31,19 @@ export const EventCategories = () => (
         <Card w="30%">
           <Image objectFit="cover" src="/assets/sports2.gif" alt="Sports GIF" />
           <CardBody textAlign="center">
-            <EventCategoryLink>Sports</EventCategoryLink>
+            <EventCategoryLink to={route.events()}>Sports</EventCategoryLink>
           </CardBody>
         </Card>
         <Card w="30%">
           <Image objectFit="cover" src="/assets/cards.gif" alt="Cards GIF" />
           <CardBody textAlign="center">
-            <EventCategoryLink>Games</EventCategoryLink>
+            <EventCategoryLink to={route.events()}>Games</EventCategoryLink>
           </CardBody>
         </Card>
         <Card w="30%">
           <Image objectFit="cover" src="/assets/galshir-dog-walker.gif" alt="Dog walking GIF" />
           <CardBody textAlign="center">
-            <EventCategoryLink>Others</EventCategoryLink>
+            <EventCategoryLink to={route.events()}>Others</EventCategoryLink>
           </CardBody>
         </Card>
       </Flex>
