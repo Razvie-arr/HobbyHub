2fdffcf604ddf884fee51fe4b8021e31941cf18b
Le Anh Viet Linh <lav.linh@gmail.com>
2024-01-20 15:45:59 +0100

    fix: fixed event cancel button showing on past events

--- frontend/src/modules/events/components/EventDetails/EventDetailsSideCard/EventDetailsSideCard.tsx
+++ frontend/src/modules/events/components/EventDetails/EventDetailsSideCard/EventDetailsSideCard.tsx
@@ -8,41 +8,46 @@ import {
   EventTypeTag,
 } from '../../../../../shared/design-system';
 import { getLocationFragmentData, WithEvent, WithNullableAuthUser } from '../../../../../shared/types';
+import { getCurrentDateTime } from '../../../../../utils/form';
 
 import { CancelEventButton } from './CancelEventButton';
 import { EventAuthor } from './EventAuthor';
 
-export const EventDetailsSideCard = ({ event, user }: WithEvent & WithNullableAuthUser) => (
-  <DataDetailsCard
-    title="Summary"
-    description={event.summary}
-    mapData={event}
-    items={[
-      {
-        icon: MdAccountCircle,
-        content: <EventAuthor event={event} />,
-      },
-      {
-        icon: MdInfo,
-        content: event.event_types.map((eventType) => <EventTypeTag key={eventType.id} eventType={eventType} />),
-      },
-      {
-        icon: MdGroups,
-        content: <EventCapacity noIcon fontSize="md" capacity={event.capacity} participants={event.participants} />,
-      },
-      {
-        icon: MdCalendarToday,
-        content: (
-          <EventDateTime noIcon fontSize="md" startDateTime={event.start_datetime} endDateTime={event.end_datetime} />
-        ),
-      },
-      {
-        icon: MdLocationOn,
-        content: <AddressInfo noIcon fontSize="md" location={getLocationFragmentData(event.location)} />,
-      },
-    ]}
-  >
-    {user && user.id === event.author.id && !event.cancelled ? <CancelEventButton event={event} /> : null}
-  </DataDetailsCard>
-);
+export const EventDetailsSideCard = ({ event, user }: WithEvent & WithNullableAuthUser) => {
+  const showCancelButton =
+    user && user.id === event.author.id && !event.cancelled && event.start_datetime.slice(0, 23) > getCurrentDateTime();
+  return (
+    <DataDetailsCard
+      title="Summary"
+      description={event.summary}
+      mapData={event}
+      items={[
+        {
+          icon: MdAccountCircle,
+          content: <EventAuthor event={event} />,
+        },
+        {
+          icon: MdInfo,
+          content: event.event_types.map((eventType) => <EventTypeTag key={eventType.id} eventType={eventType} />),
+        },
+        {
+          icon: MdGroups,
+          content: <EventCapacity noIcon fontSize="md" capacity={event.capacity} participants={event.participants} />,
+        },
+        {
+          icon: MdCalendarToday,
+          content: (
+            <EventDateTime noIcon fontSize="md" startDateTime={event.start_datetime} endDateTime={event.end_datetime} />
+          ),
+        },
+        {
+          icon: MdLocationOn,
+          content: <AddressInfo noIcon fontSize="md" location={getLocationFragmentData(event.location)} />,
+        },
+      ]}
+    >
+      {showCancelButton ? <CancelEventButton event={event} /> : null}
+    </DataDetailsCard>
+  );
+};
 
